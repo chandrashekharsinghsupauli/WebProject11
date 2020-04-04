e07cbbcb15e82a90a60cd52e34449ec83733d4d8
Chandrashekhar Singh <chandra.singh1@pearson.com>
2020-04-04 21:35:54 +0530

    test case code

---
+++ src/test/java/testcases/AllTestcase.java
@@ -0,0 +1,37 @@
+package testcases;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.java.munni.Factorial;
+
+public class AllTestcase {
+	@Test
+	public void method() {
+		System.out.println("start............");
+	}
+	@Test
+	public void munni_findFactorial5() {
+		int actualres=Factorial.findFactorial(5);
+		Assert.assertEquals(120, actualres);
+		
+	}
+	@Test
+	public void munni_findFactorial4() {
+		int actualres=Factorial.findFactorial(4);
+		Assert.assertEquals(24, actualres);
+		
+	}
+	@Test
+	public void munni_findFactorial3() {
+		int actualres=Factorial.findFactorial(3);
+		Assert.assertEquals(6, actualres);
+		
+	}
+	@Test
+	public void munni_findFactorial0() {
+		int actualres=Factorial.findFactorial(0);
+		Assert.assertEquals(1, actualres);
+		
+	}
+}
