5dfdb0f658f9c79bbca976437eb76a057eb1a56b
Chandrashekhar Singh <chandra.singh1@pearson.com>
2020-04-09 22:04:55 +0530

    multiplication code added.

--- src/main/java/com/java/munni/Calculate.java
+++ src/main/java/com/java/munni/Calculate.java
@@ -7,13 +7,17 @@ public class Calculate {
 		System.out.println("Addition of two no="+add);
 		int sub=Calculate.subtraction(5,3);
 		System.out.println("Subtration of two no="+sub);
-
+		int mult=Calculate.multiplication(20,6);
+		System.out.println("Multiplication="+mult);
 	}
 	public static int addition(int a,int b) {
 		return a+b;
 	}
 	public static int subtraction(int a,int b) {
 		return a-b;
+	}
+	public static int multiplication(int a,int b) {
+		return a*b;
 	}
 
 }
